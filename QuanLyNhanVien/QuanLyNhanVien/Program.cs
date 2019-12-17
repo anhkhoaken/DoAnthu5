a2669d792f22f95e8ff6e5c9a67a33d44b85004c
anhkhoaken <54886212+anhkhoaken@users.noreply.github.com>
2019-12-17 22:49:12 +0700

    newcmm

--- QuanLyNhanVien/QuanLyNhanVien/Program.cs
+++ QuanLyNhanVien/QuanLyNhanVien/Program.cs
@@ -16,11 +16,7 @@ namespace QuanLyNhanVien
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-<<<<<<< HEAD
             Application.Run(new QuanLyPhongBan_GUI());
-=======
-            Application.Run(new QuanLyBOPHAN_GUI());
->>>>>>> 2ca1a7c20a85b1ef5e5364b566333e19c80f86a9
         }
     }
 }
